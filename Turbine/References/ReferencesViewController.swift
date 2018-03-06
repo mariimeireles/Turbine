cf5d9cc5a77c84889b218c4c6babe3ee44847d40
Mariana Meireles <marianaribeiromeireles@gmail.com>
2018-03-06 11:13:26 -0300

    finishing references View

--- Turbine/References/ReferencesViewController.swift
+++ Turbine/References/ReferencesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ReferencesViewController: UIViewController {
-
+    
     @IBOutlet weak var reference1Label: UILabel!
     @IBOutlet weak var reference2Label: UILabel!
     @IBOutlet weak var reference3Label: UILabel!
@@ -23,8 +23,8 @@ class ReferencesViewController: UIViewController {
     
     private func setTextLabel() {
         reference1Label.text = "Fabian TC, Patton JH Jr. Croce MA, et al. Blunt Carotid arterial injuries: Importance of early diagnosis and anticoagulant therapy. An Surg 196; 23:513."
-        reference2Label.text = "Biffl M., Moore EE, Offner PJ, et al. Blunt carotid arterial injuries: implications of a new grading scale. L Trauma 1999; 47:845."
-        reference3Label.text = "Miller PR., Fabian TC., Croce MA, et al. Prospective screening for blunt cerebrovascular injuries. Ann Slurg, 2002; 22: 234."
+        reference2Label.text = "Biffl M., Moore EE, Offner PJ, et al. Blunt carotid arterial\ninjuries: implications of a new grading scale. L Trauma\n1999; 47:845."
+        reference3Label.text = "Miller PR., Fabian TC., Croce MA, et al. Prospective\nscreening for blunt cerebrovascular injuries. Ann Slurg,\n2002; 22: 234."
         rightsLabel.text = "Developed by ELSO Latin America\nin colaboration with Américas Medical Group.\nAll rights reserved © 2018"
         reference1Label.sizeToFit()
         reference2Label.sizeToFit()
