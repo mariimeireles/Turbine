0434a035568955734f0c2a56a3682f0ecf52b011
mariimeireles <marianaribeiromeireles@gmail.com>
2018-04-15 12:41:54 -0300

    taking out icon in detail view

--- Turbine/Views/Detail/DetailViewController.swift
+++ Turbine/Views/Detail/DetailViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var galleryButton: UIButton!
     var content: Content!
@@ -19,16 +18,8 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         self.title = content.title
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        setIconImage()
         setTextLabel()
         setGalleryButton()
-    }
-    
-    private func setIconImage() {
-        let imageName = content.iconImage2
-        iconImage.image = UIImage(named: imageName)
-        iconImage.layer.cornerRadius = iconImage.frame.size.width / 2
-        iconImage.layer.masksToBounds = true
     }
     
     private func setTextLabel() {
